0c6b2c450889ec61396d6336bfed8802a873890a
LuisGrigore <LuisGrigore@users.noreply.github.com>
2026-05-03 04:01:20 +0200

    user and auth services

---
+++ backend/services/auth-service/src/test/java/com/distributedshoppingsystem/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedshoppingsystem.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
